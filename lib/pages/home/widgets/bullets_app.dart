af1880c0fdfb8db8fd2bd17e3e7ca64fc4cc937a
lucas-marciano <lucas.almeida.marciano@gmail.com>
2020-05-25 10:28:43 -0300

    build menu

--- lib/pages/home/widgets/bullets_app.dart
+++ lib/pages/home/widgets/bullets_app.dart
@@ -3,45 +3,52 @@ import 'package:flutter/material.dart';
 class BulletsApp extends StatelessWidget {
   final double screenHeight;
   final int currentIndex;
+  final bool showMenu;
 
-  const BulletsApp({Key key, this.screenHeight, this.currentIndex}) : super(key: key);
+  const BulletsApp(
+      {Key key, this.screenHeight, this.currentIndex, this.showMenu})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Positioned(
-      top: screenHeight * .70,
-      child: Row(
-        children: <Widget>[
-          AnimatedContainer(
-            duration: Duration(milliseconds: 300),
-            width: 7,
-            height: 7,
-            decoration: BoxDecoration(
-              shape: BoxShape.circle,
-              color: getColor(0),
+      top: screenHeight,
+      child: AnimatedOpacity(
+        duration: Duration(milliseconds: 200),
+        opacity: showMenu ? 0 : 1,
+        child: Row(
+          children: <Widget>[
+            AnimatedContainer(
+              duration: Duration(milliseconds: 300),
+              width: 7,
+              height: 7,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: getColor(0),
+              ),
             ),
-          ),
-          SizedBox(width: 8),
-          AnimatedContainer(
-            duration: Duration(milliseconds: 300),
-            width: 7,
-            height: 7,
-            decoration: BoxDecoration(
-              shape: BoxShape.circle,
-              color: getColor(1),
+            SizedBox(width: 8),
+            AnimatedContainer(
+              duration: Duration(milliseconds: 300),
+              width: 7,
+              height: 7,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: getColor(1),
+              ),
             ),
-          ),
-          SizedBox(width: 8),
-          AnimatedContainer(
-            duration: Duration(milliseconds: 300),
-            width: 7,
-            height: 7,
-            decoration: BoxDecoration(
-              shape: BoxShape.circle,
-              color: getColor(2),
+            SizedBox(width: 8),
+            AnimatedContainer(
+              duration: Duration(milliseconds: 300),
+              width: 7,
+              height: 7,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: getColor(2),
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
--- lib/pages/home/widgets/page_view_app.dart
+++ lib/pages/home/widgets/page_view_app.dart
@@ -4,24 +4,37 @@ import 'card_app.dart';
 class PageViewApp extends StatelessWidget {
   final double screenHeight;
   final ValueChanged<int> onChangeCallback;
+  final GestureDragUpdateCallback onPanUpdate;
+  final bool showMenu;
 
-  const PageViewApp({Key key, this.screenHeight, this.onChangeCallback}) : super(key: key);
+  const PageViewApp(
+      {Key key,
+      this.screenHeight,
+      this.onChangeCallback,
+      this.onPanUpdate,
+      this.showMenu})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Positioned(
-      top: screenHeight * .25,
-      height: screenHeight * 0.45,
+    return AnimatedPositioned(
+      duration: Duration(milliseconds: 300),
+      curve: Curves.easeOut,
+      top: screenHeight,
+      height: 330,
       left: 0,
       right: 0,
-      child: PageView(
-        onPageChanged: onChangeCallback,
-        physics: BouncingScrollPhysics(),
-        children: <Widget>[
-          CardApp(),
-          CardApp(),
-          CardApp(),
-        ],
+      child: GestureDetector(
+        onPanUpdate: onPanUpdate,
+        child: PageView(
+          onPageChanged: onChangeCallback,
+          physics: showMenu ? NeverScrollableScrollPhysics() : BouncingScrollPhysics(),
+          children: <Widget>[
+            CardApp(),
+            CardApp(),
+            CardApp(),
+          ],
+        ),
       ),
     );
   }
