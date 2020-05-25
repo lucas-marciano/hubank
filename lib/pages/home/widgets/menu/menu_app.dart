d0404722860402f90ec96ce1830bf912ad9705f7
lucas-marciano <lucas.almeida.marciano@gmail.com>
2020-05-25 10:43:38 -0300

    correção do layout do menu

--- lib/pages/home/widgets/menu/menu_app.dart
+++ lib/pages/home/widgets/menu/menu_app.dart
@@ -6,106 +6,105 @@ class MenuApp extends StatelessWidget {
   final bool showMenu;
 
   const MenuApp({Key key, this.top, this.showMenu}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return Positioned(
       top: top,
       left: 0,
       right: 0,
-      height: MediaQuery.of(context).size.height * 0.6,
       child: AnimatedOpacity(
-        duration: Duration(milliseconds: 200),
+        duration: Duration(milliseconds: 100),
         opacity: showMenu ? 1 : 0,
         child: Container(
-          child: Column(
-            children: <Widget>[
-              Image.network(
-                "https://www.qrcodefacil.com/qrcodes/ff7d0bcc4726db29233cf8ceda4fd614.png?t=1590408862735",
-                height: 100,
-                color: Colors.white,
-              ),
-              Text.rich(
-                TextSpan(
-                    text: "Banco ",
+          height: MediaQuery.of(context).size.height * 0.55,
+          child: SingleChildScrollView(
+            physics: BouncingScrollPhysics(),
+            child: Column(
+              children: <Widget>[
+                Image.network(
+                  'https://webmobtuts.com/wp-content/uploads/2019/02/QR_code_for_mobile_English_Wikipedia.svg_.png',
+                  height: 100,
+                  color: Colors.white,
+                ),
+                Text.rich(
+                  TextSpan(
+                    text: 'Banco ',
                     children: [
                       TextSpan(
-                        text: "260 - Nu Pagamentos S.A.",
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
+                          text: '260 - Nu Pagamentos S.A',
+                          style: TextStyle(fontWeight: FontWeight.bold)),
                     ],
-                    style: TextStyle(fontSize: 12)),
-              ),
-              SizedBox(height: 5),
-              Text.rich(
-                TextSpan(
-                    text: "Agência ",
+                  ),
+                  style: TextStyle(fontSize: 12),
+                ),
+                SizedBox(
+                  height: 5,
+                ),
+                Text.rich(
+                  TextSpan(
+                    text: 'Agência ',
                     children: [
                       TextSpan(
-                        text: "000000",
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
+                          text: '0001',
+                          style: TextStyle(fontWeight: FontWeight.bold)),
                     ],
-                    style: TextStyle(fontSize: 12)),
-              ),
-              SizedBox(height: 5),
-              Text.rich(
-                TextSpan(
-                    text: "Conta ",
+                  ),
+                  style: TextStyle(fontSize: 12),
+                ),
+                SizedBox(
+                  height: 5,
+                ),
+                Text.rich(
+                  TextSpan(
+                    text: 'Conta ',
                     children: [
                       TextSpan(
-                        text: "0000-00",
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
+                          text: '0000000-0',
+                          style: TextStyle(fontWeight: FontWeight.bold)),
                     ],
-                    style: TextStyle(fontSize: 12)),
-              ),
-              SizedBox(height: 25),
-              SingleChildScrollView(
-                child: Padding(
+                  ),
+                  style: TextStyle(fontSize: 12),
+                ),
+                SizedBox(
+                  height: 25,
+                ),
+                Padding(
                   padding: const EdgeInsets.symmetric(horizontal: 30),
                   child: Column(
-                    children: [
+                    children: <Widget>[
                       ItemMenu(
                         icon: Icons.info_outline,
-                        text: "Me ajuda",
+                        text: 'Me ajuda',
                       ),
                       ItemMenu(
                         icon: Icons.person_outline,
-                        text: "Perfil",
+                        text: 'Perfil',
                       ),
                       ItemMenu(
                         icon: Icons.settings,
-                        text: "Configurar conta",
+                        text: 'Configurar conta',
                       ),
                       ItemMenu(
                         icon: Icons.credit_card,
-                        text: "Configurar cartão",
+                        text: 'Configurar Cartão',
                       ),
                       ItemMenu(
                         icon: Icons.store_mall_directory,
-                        text: "Pedir conta PJ",
+                        text: 'Pedir conta PJ',
                       ),
                       ItemMenu(
                         icon: Icons.phone_android,
-                        text: "Configurações do app",
+                        text: 'Configurações do app',
                       ),
-                      SizedBox(height: 25),
+                      SizedBox(
+                        height: 25,
+                      ),
                       Container(
-                        height: 35,
+                        height: 40,
                         width: double.infinity,
                         decoration: BoxDecoration(
-                          border: Border.all(
-                            width: 0.7,
-                            color: Colors.white54,
-                          ),
-                        ),
+                            border:
+                                Border.all(width: 0.7, color: Colors.white54)),
                         child: RaisedButton(
                           color: Colors.purple[800],
                           highlightColor: Colors.transparent,
@@ -115,18 +114,19 @@ class MenuApp extends StatelessWidget {
                           highlightElevation: 0,
                           hoverElevation: 0,
                           splashColor: Colors.purple[900],
-                          onPressed: () {},
                           child: Text(
-                            "SAIR DO APP",
-                            style: TextStyle(fontSize: 10),
+                            'SAIR DO APP',
+                            style: TextStyle(
+                                fontSize: 10, fontWeight: FontWeight.bold),
                           ),
+                          onPressed: () {},
                         ),
                       ),
                     ],
                   ),
-                ),
-              ),
-            ],
+                )
+              ],
+            ),
           ),
         ),
       ),
